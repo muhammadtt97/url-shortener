aee3dd926bc37f92a6a903467aadf386c19855c7
Muhammad Al-ameen <muhammadtt97@gmail.com>
2023-09-24 13:44:05 +0100

    Update ShortenerForm.js

--- url-shortener/src/components/ShortenerForm.js
+++ url-shortener/src/components/ShortenerForm.js
@@ -9,28 +9,34 @@ function ShortenerForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        // Validate the URL format
-        if (!isValidURL(longUrl)) {
-          setError('Please enter a valid URL.');
-          return;
-        }
+      // Validate the URL format
+      if (!isValidURL(longUrl)) {
+        setError('Please enter a valid URL.');
+        return;
+      }
+      
+      // Send a request to your backend to generate a short URL.
+      // For now, simulate this with a dummy short URL.
+      setShortUrl('https://your-shortened-url.com/abcd123');
+      setError('');
+    } catch (err) {
+      setError('An error occurred. Please try again.');
+    }
+  };
 
-    setShortUrl('https://your-shortened-url.com/abcd123');
-    setError('');
-        } catch (err) {
-         setError('An error occurred. Please try again.');
-        }
-    };
+  const isValidURL = (url) => {
+    // Basic URL validation regex (you can use a more robust one)
+    const pattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+    return pattern.test(url);
+  };
 
-    const isValidURL = (url) => {
-        // Basic URL validation regex (you can use a more robust one)
-        const pattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
-        return pattern.test(url);
-      };
-    
+  const handleCopyToClipboard = () => {
+    // Implement copy-to-clipboard functionality here
+    // You can use the Clipboard API or a library like "clipboard.js"
+  };
 
   return (
-    <div>
+    <div className="form-container">
       <form onSubmit={handleSubmit}>
         <input
           type="url"
@@ -41,12 +47,14 @@ function ShortenerForm() {
         />
         <button type="submit">Shorten URL</button>
       </form>
+      {error && <div className="error">{error}</div>}
       {shortUrl && (
         <div>
           <p>Shortened URL:</p>
-          <a href={shortUrl} target="_blank" rel="noopener noreferrer">
-            {shortUrl}
-          </a>
+          <div className="shortened-url">
+            <span>{shortUrl}</span>
+            <button onClick={handleCopyToClipboard}>Copy to Clipboard</button>
+          </div>
         </div>
       )}
     </div>
