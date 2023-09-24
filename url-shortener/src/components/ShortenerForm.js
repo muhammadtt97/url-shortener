dcb49c4656c576ae9e88c66ea2fc5441d2557e33
Muhammad Al-ameen <muhammadtt97@gmail.com>
2023-09-24 13:21:38 +0100

    Update ShortenerForm.js
    added error handling function

--- url-shortener/src/components/ShortenerForm.js
+++ url-shortener/src/components/ShortenerForm.js
@@ -4,13 +4,30 @@ import './ShortenerForm.css';
 function ShortenerForm() {
   const [longUrl, setLongUrl] = useState('');
   const [shortUrl, setShortUrl] = useState('');
-  
+  const [error, setError] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+    try {
+        // Validate the URL format
+        if (!isValidURL(longUrl)) {
+          setError('Please enter a valid URL.');
+          return;
+        }
+
     setShortUrl('https://your-shortened-url.com/abcd123');
-  };
+    setError('');
+        } catch (err) {
+         setError('An error occurred. Please try again.');
+        }
+    };
+
+    const isValidURL = (url) => {
+        // Basic URL validation regex (you can use a more robust one)
+        const pattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+        return pattern.test(url);
+      };
+    
 
   return (
     <div>
